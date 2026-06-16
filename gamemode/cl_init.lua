2308a29d96a9fdd69d0303c8668a89ea9a10b0c1
Nyaaa <5182757+oNyaaa1@users.noreply.github.com>
2026-06-16 09:54:33 +0100

    update

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,8 +1,3 @@
 include("shared.lua")
 local DungeonGenerate = "Inventory/"
 include(DungeonGenerate .. "cl_init.lua")
-surface.CreateFont("GModWorldtip", {
-    font = "Helvetica",
-    size = 20,
-    weight = 700
-})
